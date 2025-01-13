0eb4dcc3ce2f584f174f2de8a90d673ce9432890
unknown <feriel.benkraiem@ensi-uma.tn>
2025-01-13 10:00:20 +0100

    front

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -79,7 +79,7 @@ export default function OrdersPage() {
                       <td className="p-3 border">{order.monitoringTool || "N/A"}</td>
                       <td className="p-3 border">{order.hostingJarTool || "N/A"}</td>
                       <td className="p-3 border">
-                      <span className={`py-1 px-3 rounded text-white ${order.status === "en attente" ? "bg-yellow-500" : "bg-gray-400"}`}>{order.status}</span>
+                      <span className={`py-1 px-3 rounded text-white ${order.status === "en attente" ? "bg-yellow-500" : "bg-green-400"}`}>{order.status}</span>
                     </td>                          
                   </tr>
                   ))
